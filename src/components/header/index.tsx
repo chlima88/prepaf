3f5799d52ba8734f0a72acefc2547362f9bb1b1d
Charles Costa <chlima88@gmail.com>
2023-10-27 21:35:49 -0300

    chore: Here we go again...

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <>
+            <div>
+                <div className="logo">aaaaaasda</div>
+                <div className="menu">asdas</div>
+                <div className="user">asdasd</div>
+            </div>
+        </>
+    );
+}
