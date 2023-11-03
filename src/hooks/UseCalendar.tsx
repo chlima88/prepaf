8cc74c36779dd6d031391ec99137271629af64c6
Charles Costa <chlima88@gmail.com>
2023-11-03 17:57:32 -0300

    refactor: reduce the number of for loops in UseCalendar component

--- src/hooks/UseCalendar.tsx
+++ src/hooks/UseCalendar.tsx
@@ -12,81 +12,49 @@ export function useCalendar() {
 
     function createCalendar() {
         const GRIDSIZE = 49;
-        const date = new Date(year, month);
         const calendarDays = [];
+        const firstWeekDay = new Date(year, month, 1).getDay();
+        const lastMonthDay = new Date(year, month + 1, 0).getDate();
 
-        const firstWeekDay = new Date(
-            date.getFullYear(),
-            date.getMonth(),
-            1
-        ).getDay();
-
-        const lastMonthDay = new Date(
-            date.getFullYear(),
-            date.getMonth() + 1,
-            0
-        ).getDate();
-
-        for (let i = 0; i < 7; i++) {
-            const weekday = [
-                "Domingo",
-                "Segunda",
-                "Terça",
-                "Quarta",
-                "Quinta",
-                "Sexta",
-                "Sabado",
-            ];
+        [
+            "Domingo",
+            "Segunda",
+            "Terça",
+            "Quarta",
+            "Quinta",
+            "Sexta",
+            "Sabado",
+        ].map((weekday) => {
             calendarDays.push(
                 <p
                     key={calendarDays.length}
                     className={` text-center font-semibold min-w-[50px] h-10 p-2 truncate`}
                 >
-                    {weekday[i]}
+                    {weekday}
                 </p>
             );
-        }
+        });
 
         for (
             let dayCounter = firstWeekDay * -1 + 1;
-            dayCounter <= 0;
+            calendarDays.length < GRIDSIZE;
             dayCounter++
         ) {
             const date = new Date(year, month, dayCounter).valueOf();
             let itemsPosition = "center";
-            if (dayCounter == firstWeekDay * -1 + 1) itemsPosition = "start";
-
-            calendarDays.push(
-                <CalendarDay
-                    date={date}
-                    key={calendarDays.length}
-                    itemsPosition={itemsPosition}
-                    filler
-                />
-            );
-        }
-
-        for (let dayCounter = 1; dayCounter <= lastMonthDay; dayCounter++) {
-            const date = new Date(year, month, dayCounter).valueOf();
-
-            let itemsPosition = "center";
             if (calendarDays.length % 7 == 0) itemsPosition = "start";
             if ((calendarDays.length + 1) % 7 == 0) itemsPosition = "end";
 
+            let filler = true;
+            if (dayCounter > 0 && dayCounter <= lastMonthDay) filler = false;
+
             calendarDays.push(
                 <CalendarDay
-                    key={calendarDays.length}
                     date={date}
+                    key={calendarDays.length}
                     itemsPosition={itemsPosition}
+                    filler={filler}
                 />
-            );
-        }
-
-        for (let dayCounter = 1; calendarDays.length < GRIDSIZE; dayCounter++) {
-            const date = new Date(year, month + 1, dayCounter).valueOf();
-
-            calendarDays.push(
-                <CalendarDay date={date} key={calendarDays.length} filler />
             );
         }
 
